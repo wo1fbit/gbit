e35dff6de0ad2669a9f654a1988310ac6009afa3
wo1fbit <shellbash0@gmail.com>
2022-08-18 08:57:05 +0300

    Added pkexec

---
+++ src/gbit/bin/pkexec.py
@@ -0,0 +1,11 @@
+from gbit.var import *
+
+gtfobin = f"""
+{SUDO}
+{line}
+{colored("If the binary is allowed to run as superuser by sudo, it does not drop the elevated privileges and", 'blue')}
+{colored("may be used to access the file system, escalate or maintain privileged access.", 'blue')}
+{colored("    sudo pkexec /bin/sh", 'green')}
+
+{colored("cheers, wolf 🐺.", 'red', 'on_yellow', attrs=['bold', 'dark'])}
+"""
